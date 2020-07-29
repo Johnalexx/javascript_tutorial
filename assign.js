3daa5c3624f477af582ffb77feae89b11b7f730b
anchor <anchor@localhost.localdomain>
2020-07-29 21:17:17 +0100

    Assigns and also ask

--- assign.js
+++ assign.js
@@ -1,2 +1,5 @@
 a = 2;              //assign the value 2 to a
 console.log(a);        //prints the value given in the browser console
+
+//Prompt
+var age = prompt("How old are you?")        //asks you for your age 
