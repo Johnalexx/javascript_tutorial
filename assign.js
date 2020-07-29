52a03ae45958f05d8877210059c57b51d42e6585
anchor <anchor@localhost.localdomain>
2020-07-29 21:25:38 +0100

    assigning and prompt

--- assign.js
+++ assign.js
@@ -3,5 +3,5 @@ console.log(a);        //prints the value given in the browser console
 
 //Prompt
 var age = prompt("How old are you?")        //asks you for your age 
-alert(age)                                  //pops up an alert of your inputed value
-console.log(age)                            //prints your value in the browser console
+alert(age);                                  //pops up an alert of your inputed value
+console.log(age);                            //prints your value in the browser console
