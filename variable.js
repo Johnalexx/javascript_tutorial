a93fc766b87223358513f6b4c3a3d7e4b0ea8b02
anchor <anchor@localhost.localdomain>
2020-07-31 18:23:33 +0100

    variable

---
+++ variable.js
@@ -0,0 +1,26 @@
+//ways to declare variable in javascript
+
+//the var
+var a = 20;
+//the let
+let b = 10;
+//the const
+const c = 5;
+//the function
+function d()  {}
+
+/*Block
+a block is a pairing of curly braces with one or more statement
+Example
+*/
+
+/*{
+    what is inside this is a BLOCK
+}
+*/
+
+var a = 40;
+if (a > 10){
+    a = 10
+    console.log(a);
+}
