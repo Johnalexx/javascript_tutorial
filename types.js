55e4378943f67e0060b2d8b570dce3fbbde4764e
anchor <anchor@localhost.localdomain>
2020-07-31 16:47:19 +0100

    Strings and Integer

---
+++ types.js
@@ -0,0 +1,22 @@
+//Types -Strings and Integers
+
+a = 12;         //defines 'a' as 12
+
+a = a / 2;          //divides the previous value of 'a' and change it to 6
+
+console.log(a);     //prints 6 on the console as an integer
+
+//printing integers as strings
+
+a = 12;                         //defines 'a' as 12
+
+a = a / 2;                      //divides the previous value of 'a' and change it to '6'
+
+a = String(a);                  //this gives 'a' the ability to print as a string
+
+console.log(a);                 //prints 6 on the console as a string
+
+/*To differentiate between the string and integer on the console is that 
+The String prints in colour BLACK on the console while
+The Integer prints in colour BLUE on the console.
+*/
