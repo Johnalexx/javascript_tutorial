def6c0a39dc1b66c3eb266df6b39215e0ea81d2f
anchor <anchor@localhost.localdomain>
2020-08-03 15:43:44 +0100

    Conditions

---
+++ conditions.js
@@ -0,0 +1,12 @@
+/*In Javascript there are two types of values
+
+--> The False(The Falsy) Value - are values which give invalid response such as:
+        - 0 -0 Nan
+        - ""
+        - Undefined
+        - Null
+        -False
+
+
+--> The Truth(The Truthsy) Value - 
+*/
