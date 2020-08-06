549796790f0551047938f202677d6d33fd2d670e
Johnalexx <adegbolajohn1@gmail.com>
2020-08-06 08:37:29 +0100

    Function

---
+++ function.js
@@ -0,0 +1,13 @@
+function a(){
+                    //these are function declaration
+    }
+
+
+        //these are function expressionS
+var a = function(){
+                
+}
+
+var a = function b(){
+    
+}
