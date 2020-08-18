358d545ff8d0f6409f17511613336c3a8eb29a28
Johnalexx <adegbolajohn1@gmail.com>
2020-08-18 14:24:53 +0100

    Function Expressions

---
+++ intermediate/scope/functionexpression.js
@@ -0,0 +1,25 @@
+//FUNCTION EXPRESSION
+/*  There are two function expressions
+        --> Named Function Expression
+        --> Anonymous Function expression
+        
+        ***IT IS ADVISEABLE TO USE THE NAMED FUNCTION Over the ANONYMOUS FUNCTION EXPRESSION*** 
+
+          |----------------------------------------------------------------|
+          |Benefits/Reason to pick Named Function over anonymous expression|
+          |--------------------------------------------------------------- |  
+                        -   it provides a handy self-reference function from itself
+                        -   it is easily traceable
+                        -   more debuggable stack traces
+                        -   more understandable and accessible
+                        -    (declaration and expression) when a function is named it makes it easier for us to consume it again for similar use cases.
+                        
+        */
+
+var food = function food{
+    //This is a Named function expression
+}
+
+var music = function(){
+    //This is an anonymous function expression
+}
